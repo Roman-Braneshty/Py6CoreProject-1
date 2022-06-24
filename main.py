c2d4f0709af4d83c2181d2cf4ec42b05498abaec
Roman-Braneshty <102828727+Roman-Braneshty@users.noreply.github.com>
2022-06-24 12:07:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ from Notates import *
 
 
 def main():
-    pass
+    contacts = reading_db(file_name)
+    print('Выберите направление работы помощника: Notate - Введите(N) | AdressBook - Введите(A) ')
+    branch = input('>>> ')
+    if branch == 'A':
+        while True:
+            user_command = input('>>> ')
+            command, data = command_parser(user_command)
+            print(command(contacts, *data))
+            if command is exiting:
+                break
+    elif branch == 'N':
+        pass
+
+    else:
+        print('Нет доступной работы,выберите работу заново')
 
 
 if __name__ == '__main__':
