750d9b28d5b3f9cf5fadfb9067f3bcccb1a63508
romaniuk-o <O.romaniuk54@gmail.com>
2022-06-25 18:03:08 +0300

    Update AdressBook.py

--- AdressBook.py
+++ AdressBook.py
@@ -24,8 +24,10 @@ class MailExists(Exception):
 class IncorrectEmailFormat(Exception):
     pass
 
+
 class PhoneNumberError(Exception):
     pass
+
 
 class Phone(Field):
     def __init__(self, value) -> None:
